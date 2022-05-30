c24f9392c4ff5ac80c16bd9c02ee7eaca9ab5495
omarzabin <97465251+omarzabin@users.noreply.github.com>
2022-05-30 12:28:12 +0300

    9

--- app/src/main/java/com/example/petservice/MainActivity.kt
+++ app/src/main/java/com/example/petservice/MainActivity.kt
@@ -38,20 +38,25 @@ class MainActivity : AppCompatActivity() {
         var options = arrayOf("Check-up", "Vaccine", "Dental cleaning", "Bath Services")
         spinnerVal.adapter =
             ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
-        resultBT.setOnClickListener { view ->
-            if (flag == "Check-up")
-                resultTV.text = 50.toString();
-            else if (flag == "Vaccine")
-                resultTV.text = 15.toString();
-            else if (flag == "Dental cleaning")
-                resultTV.text = 70.toString();
-            else if (flag == "Bath Services")
-                resultTV.text = 10.toString();
-        }
+
         spinnerVal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 flag = options.get(p2)
+
+                resultBT.setOnClickListener { view ->
+                    if (flag == "Check-up")
+                        resultTV.text = 50.toString()
+                    else if (flag == "Vaccine")
+                        resultTV.text = 15.toString()
+                    else if (flag == "Dental cleaning")
+                        resultTV.text = 70.toString()
+                    else if (flag == "Bath Services")
+                        resultTV.text = 10.toString()
+                   // Toast.makeText(this,"Test",Toast.LENGTH_LONG).show()
+                }
+
             }
+
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
 
